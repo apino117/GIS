c7b52375d95f3e4b3282f53ea2c682d238548ade
Alex Pino <alex@Alexs-MacBook-Pro.local>
2019-06-12 11:48:33 -0400

    copied from original repo https://github.com/Edpaynter/groupProject2

---
+++ config/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "highscore_db",
+    host: "localhost",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
+  },
+  production: {
+    // eslint-disable-next-line camelcase
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
---
+++ models/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS highscore_db;
+CREATE DATABASE highscore_db;
+
+DROP DATABASE IF EXISTS test_score_db;
+CREATE DATABASE test_score_db;
